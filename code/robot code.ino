3c896a617409767e7fca3d0f1f15a81ae43add23
kakhi-kotchauri <87081106+kakhi-kotchauri@users.noreply.github.com>
2022-11-20 21:34:31 +0400

    Add files via upload

--- code/robot code.ino
+++ code/robot code.ino
@@ -62,6 +62,8 @@ bool isturning = false;
 bool turnleftactive = false;
 
 
+
+
 int deg;
 
 
@@ -84,6 +86,29 @@ int leftturndistance;
 int rightturndistance;
 
 
+// for useautonomy
+bool blockreset = false;
+bool stopcheckaround = true;
+bool callstoper = true;
+bool stop1 = true;
+bool stop2 = true;
+bool stop3 = true;
+bool stop4 = true;
+bool stoper1 = false;
+bool moveblocker = false;
+bool once = false;
+
+int leftdistance;
+int rightdistance;
+
+AsyncDelay delay1;
+AsyncDelay delay2;
+AsyncDelay delay3;
+AsyncDelay delay4;
+AsyncDelay reversingtimer1;
+AsyncDelay reversingtimer2;
+AsyncDelay turningtimer;
+
 
 
 // for compass
@@ -97,19 +122,14 @@ QMC5883LCompass compass;
 // functions for distance measure
 
 void usesonic() {
-  // ultrasound
-  // Clears the trigPin
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
     distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
   // Serial.print("Distance: ");
 }
 
@@ -119,13 +139,13 @@ void useaveragenum(int measurements, int number) {
 for(int i = 0; i < measurements; ++i)
 {
   calculate += number;
-  delay(1);
+  delay(0.3);
 }
 
 calculate /= measurements;
 
 
-if(calculate >= 0) {
+if(calculate >= 0 && calculate < 200) {
   averagedistance = calculate;
 }
 
@@ -211,7 +231,9 @@ void reset() {
  {
     correctheading = azimuth;
     headingsetted = true;
-    Serial.println(00000000000);
+    movingonright = false;
+    movingonleft = false;
+    Serial.println("RR");
     Serial.println(azimuth);
  }
 
@@ -227,7 +249,7 @@ void reset() {
     backward(false); 
     right(false); 
     left(false); 
-    if(!turnrightactive && turnleftactive) {
+    if(!turnrightactive && !turnleftactive) {
       correctheading = azimuth;
     }
     headingtimer.start(170, AsyncDelay::MILLIS);
@@ -236,8 +258,6 @@ void reset() {
     backwardtimer.start(180, AsyncDelay::MILLIS);
     blocktimercheck = false;
     irtimeractive = false;
-    movingonright = false;
-    movingonleft = false;
     isreseted = true;
     headingsetted = false;
     rightturndistance = 0;
@@ -256,34 +276,36 @@ bool testt2 = false;
 
 void turnright() { 
 
- if(azimuth < correctheading + deg && turnrightactive && !testt) {
+ if(azimuth < correctheading + deg && turnrightactive && !testt && turningtimer.isExpired() == false) {
 
-   if(azimuth < 5 && correctheading + deg - 359 >= 0 ) {
+   if(azimuth < 10 && azimuth >= 5 && correctheading + deg - 359 >= 0 ) {
      correctheading = correctheading + deg - 359;
     //  Serial.println("metia 359 ze");
-     Serial.println(correctheading);
+    //  Serial.println(correctheading);
      testt = true;
    }
 
    right(true);
    isturning = true;
-   Serial.println("turningonrightside");
- } else if(azimuth < correctheading && testt) {
+  //  Serial.println("turningonrightside");
+
+ } else if(azimuth < correctheading && testt && turningtimer.isExpired() == false) {
    Serial.println(correctheading);
    Serial.println(azimuth);
-       right(true);
+   right(true);
    isturning = true;
-  //  Serial.println("turningonrightside2");
- }
- else if(!turnedonright) {
+
+ } else if(!turnedonright) {
    right(false);
   //  correctheading = azimuth;
    isturning = false;
   //  Serial.println("finished turning on right side");
+   moveblocker = false;
    turnedonright = true;
    testt = false;
    turnrightactive = false;
-  headingtimer.start(170, AsyncDelay::MILLIS);
+   headingtimer.start(150, AsyncDelay::MILLIS);
+   headingsetted = false;
  }
 
 }
@@ -292,34 +314,37 @@ void turnright() {
 
 void turnleft() { 
 
- if(azimuth > correctheading - deg && turnleftactive && !testt2) {
+ if(azimuth > correctheading - deg && turnleftactive && !testt2 && turningtimer.isExpired() == false) {
 
-   if(azimuth > 355 && correctheading - deg + 359 >= 0 ) {
+   if(azimuth >= 350 && azimuth < 355 && correctheading - deg + 359 <= 350) {
      correctheading = correctheading - deg + 359;
     //  Serial.println("naklebia 359 ze");
      Serial.println(correctheading);
      testt2 = true;
    }
-
+ 
    left(true);
    isturning = true;
-  //  Serial.println("turningonrightside");
- } else if(azimuth > correctheading && testt2) {
+  //  Serial.println("leftside");
+   Serial.println(azimuth);
+
+ } else if(azimuth > correctheading && testt2 && turningtimer.isExpired() == false) {
+
    Serial.println(correctheading);
    Serial.println(azimuth);
-       left(true);
+   left(true);
    isturning = true;
-  //  Serial.println("turningonrightside2");
- }
- else if(!turnedonleft) {
+
+ } else if(!turnedonleft) {
    left(false);
-  //  correctheading = azimuth;
    isturning = false;
   //  Serial.println("finished turning on left side");
+   moveblocker = false;
    turnedonleft = true;
    testt2 = false;
    turnleftactive = false;
-  headingtimer.start(170, AsyncDelay::MILLIS);
+   headingtimer.start(150, AsyncDelay::MILLIS);
+   headingsetted = false;
  }
 
 }
@@ -328,11 +353,11 @@ void turnleft() {
 void reciveir() {
 
 if (irrecv.decode(&results)){
+
     irrecv.resume();
     isreseted = false;
     irtimer.start(110, AsyncDelay::MILLIS);
     
-
     if(results.value != 4294967295) {
         oldir = results.value;
     }
@@ -341,73 +366,64 @@ if (irrecv.decode(&results)){
 
     if(oldir == -12241) {
       forward(true); 
-      // Serial.println("forward");
     } else if(oldir == 28815) {
       backward(true);
-      // Serial.println("backward");
-    } else if(oldir == -30601 && blockreverse) {
-
-       if(setmode == 0 && !blocktimercheck) {
-          headservo.write(0);
-          servotimer.start(200, AsyncDelay::MILLIS);
-          blocktimercheck = true;
-       }
-
-       if(servotimer.isExpired() && headservo.read() == 0) {
-          rightturndistance = averagedistance;
-       }
-
-       if( rightturndistance >= 22) {
-          headservo.write(90);
-          right(true);
-          movingonright = true;
-          correctheading = azimuth;
-          // Serial.println("right");
-       } else if(setmode == 1) {
-          right(true);
-          // Serial.println("right");
-       }
-
+    } else if(oldir == -30601 && blockreverse) {      
+      if(setmode == 0 && !blocktimercheck) {
+        headservo.write(0);
+        servotimer.start(200, AsyncDelay::MILLIS);
+        blocktimercheck = true;
+      }
+      if(servotimer.isExpired() && headservo.read() == 0) {
+        rightturndistance = averagedistance;
+      }
+      if( rightturndistance >= 22) {
+        headservo.write(90);
+        right(true);
+        movingonright = true;
+        correctheading = azimuth;
+      } else if(setmode == 1) {
+        right(true);
+      }
     } else if(oldir == 2295 && blockreverse) {
-      // left(true);
-
-        if(setmode == 0 && !blocktimercheck) {
-          headservo.write(180);
-          servotimer.start(200, AsyncDelay::MILLIS);
-          blocktimercheck = true;
-       }
-
-        if(servotimer.isExpired() && headservo.read() == 180) {
-            leftturndistance = averagedistance;
-       }
-
-       if(leftturndistance >= 22) {
-          headservo.write(90);
-          left(true);
-          movingonleft = true;
-          correctheading = azimuth;
-          // Serial.println("left");
-       } else if(setmode == 1) {
-          left(true);
-          // Serial.println("left");
-       }
-
+      if(setmode == 0 && !blocktimercheck) {
+        headservo.write(180);
+        servotimer.start(200, AsyncDelay::MILLIS);
+        blocktimercheck = true;
+      }
+      if(servotimer.isExpired() && headservo.read() == 180) {
+          leftturndistance = averagedistance;
+      }
+      if(leftturndistance >= 22) {
+        headservo.write(90);
+        left(true);
+        movingonleft = true;
+        correctheading = azimuth;
+      } else if(setmode == 1) {
+        left(true);
+      }
     } else if(oldir == 6375) {
       setmode = 0;
     } else if(oldir == 26775) {
       setmode = 1;
       digitalWrite(ledpin, HIGH);
       stopforward = false;
+    } else if (oldir == -26521) {
+       setmode = 2;
     } else if(oldir == -17851) {
       digitalWrite(ledpin, LOW);
     } else if(oldir == 30855) {
       deg = 90;
       turnrightactive = true;
       turnedonright = false;
+      correctheading = azimuth;
+      turningtimer.start(10000, AsyncDelay::MILLIS);
     } else if(oldir == 14535) {
       deg = 90;
       turnleftactive = true;
       turnedonleft = false;
+      correctheading = azimuth;
+      turningtimer.start(10000, AsyncDelay::MILLIS);
     }
 
 
@@ -423,7 +439,6 @@ if (irrecv.decode(&results)){
 // for blinkled
 
 AsyncDelay ledtimer;
-
 
 void blinkled(int timedelay) {
 
@@ -443,6 +458,10 @@ void blinkled(int timedelay) {
 
 
 // function for hybrid mode
+int maxtolerance = 18;
+int tolerance = 18;
+
+AsyncDelay correctiontimer;
 
 void usehybridmode() {
  
@@ -462,41 +481,49 @@ void usehybridmode() {
       correctheading = 355;
     }
 
-    if(correctheading + 18 < azimuth && azimuth <= 180) {
+    if(correctheading + tolerance < azimuth && azimuth <= 180 ) {
       left(true);
+      tolerance = 0;
       rightcorrected = false;
       // Serial.println("leftcorrect1");
       // Serial.println(correctheading);
       // Serial.println(azimuth);
-    } else if(!rightcorrected) {
+    } else if(!rightcorrected) { 
       left(false);
+      tolerance = maxtolerance;
       rightcorrected = true;
     }
 
-    if(correctheading - 18 > azimuth && azimuth <= 180) {
+    if(correctheading - tolerance > azimuth && azimuth <= 180 ) {
       right(true);
+      tolerance = 0;
       leftcorrected = false;
     } else if(!leftcorrected) {
       right(false);
+      tolerance = maxtolerance;
       leftcorrected = true;
     }
 
 
-    if(correctheading + 18 < azimuth && azimuth > 180) {
+    if(correctheading + tolerance < azimuth && azimuth > 180 ) {
       left(true);
+      tolerance = 0;
       // Serial.println("leftcorrect2");
       rightcorrected2 = false;
     } else if(!rightcorrected2) {
       left(false);
+      tolerance = maxtolerance;
       rightcorrected2 = true;
     }
 
 
-    if(correctheading - 18 > azimuth && azimuth > 180) {
+    if(correctheading - tolerance > azimuth && azimuth > 180 ) {
       right(true);
+      tolerance = 0;
       leftcorrected2 = false;
     } else if(!leftcorrected2) {
       right(false);
+      tolerance = maxtolerance;
       leftcorrected2 = true;
     }
 
@@ -507,7 +534,7 @@ void usehybridmode() {
 /////////
 
 
-    if(averagedistance  <= 20 && !stopforward && headservo.read() == 90) {
+    if(averagedistance  <= 20 && !stopforward && headservo.read() == 90 || digitalRead(6) == 0 && !stopforward && headservo.read() == 90) {
       // Serial.println("gacherdi");
       forward(false);
       stopforward = true;
@@ -525,6 +552,147 @@ void usehybridmode() {
       backward(false);
       blockreverse = true;
     }
+
+
+}
+
+
+
+
+// useautonomy
+
+void useautonomy() {
+
+    blinkled(1000);
+
+    if(!moveblocker && !once) {
+      forward(true);
+      once = true;
+      // Serial.println("gaxsna");
+    } else if(moveblocker && once) {
+      forward(false);
+      once = false;
+      // Serial.println("dabloka");
+    }
+
+
+
+
+  if(averagedistance < 20 && callstoper && !isturning || digitalRead(6) == 0 && callstoper && !isturning) {
+
+    stopcheckaround = false; 
+    moveblocker = true;
+    callstoper = false;
+    leftdistance = 0;
+    rightdistance = 0;
+    // Serial.println("charte tavidan");
+
+  } else if(averagedistance >= 30 && stopcheckaround && !callstoper ) {
+    callstoper = true;
+    // Serial.println("gaachere");
+  }
+
+    if(averagedistance < 20 || digitalRead(6) == 0) {
+      forward(false);
+      stopforward = true;
+      moveblocker = true;
+    // Serial.println("tormuzi");
+    } else if(averagedistance >= 20 && stopforward && stopcheckaround) {
+    // Serial.println("tormuzis gauqmeba");
+      stopforward = false;
+    }
+
+  if(!stopcheckaround) {
+
+  
+    if(!blockreset) {
+   
+      stop1= false;
+      blockreset = true;
+      delay1.start(500, AsyncDelay::MILLIS);
+      delay2.start(1000, AsyncDelay::MILLIS);
+      delay3.start(1500, AsyncDelay::MILLIS);
+      delay4.start(2000, AsyncDelay::MILLIS);
+    }
+
+  
+    if(delay1.isExpired() && !stop1) {
+        headservo.write(25);
+      // Serial.println("a");
+      stop1 = true;
+      stop2 = false;
+    }
+
+    if(delay2.isExpired() && !stop2) {
+      rightdistance = averagedistance;
+        headservo.write(90);
+      Serial.println("R");
+      Serial.println(rightdistance);
+      stop2 = true;
+      stop3 = false;
+    }
+
+    if(delay3.isExpired() && !stop3) {
+        headservo.write(155);
+      // Serial.println("c");
+      stop3 = true;
+      stop4 = false;
+    }
+
+    if(delay4.isExpired() && !stop4) {
+      leftdistance = averagedistance;
+        headservo.write(90);
+      Serial.println("L");
+      Serial.println(leftdistance);
+      stop4 = true;
+      delay(700);
+      // averagedistance = 0;
+      blockreset = false;
+      stoper1 = false;
+      backward(true);
+      reversingtimer1.start(200, AsyncDelay::MILLIS);
+      // Serial.println("zadni");
+      stopcheckaround = true;
+    }
+    
+
+  }
+
+
+ if(rightdistance > 0 && leftdistance > 0 && stoper1 && reversingtimer2.isExpired()) {
+
+//  Serial.println("shemovida aq");
+  correctheading = azimuth;
+
+  if(rightdistance >= leftdistance) {
+    // Serial.println("mibrundi1");
+      deg = 50;
+      correctheading = azimuth;
+      turningtimer.start(10000, AsyncDelay::MILLIS);
+      turnrightactive = true;
+      turnedonright = false;
+      leftdistance = 0;
+      rightdistance = 0;
+  } else if(leftdistance > rightdistance) {
+    // Serial.println("mibrundi2");
+      deg = 50;
+      correctheading = azimuth;
+      turningtimer.start(10000, AsyncDelay::MILLIS);
+      turnleftactive = true;
+      turnedonleft = false;
+      leftdistance = 0;
+      rightdistance = 0;
+  }
+
+}
+
+if(reversingtimer1.isExpired() && !stoper1) {
+  stoper1 = true;
+  backward(false);
+  reversingtimer2.start(200, AsyncDelay::MILLIS);
+  // Serial.println("gachereba");
+} 
+
 
 
 }
@@ -559,6 +727,9 @@ void setup()
 
 
 
+
+
+
 void loop(){
 
   if(setmode == 0) { 
@@ -568,8 +739,17 @@ void loop(){
     usehybridmode();
     turnright();
     turnleft();
+  } else if(setmode == 2) {
+    useautonomy();
+    usecompass();
+    usesonic();
+    useaveragenum(30, distance);
+    turnright();
+    turnleft();
   }
 
   reciveir();
 
+//  Serial.println(averagedistance);
+    
 }
